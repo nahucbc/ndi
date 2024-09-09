0ac8dc62f66a8be5c7b721634692bd173b146584
Nahuel Alexis Medina Llanos <nahu.cbc@gmail.com>
2024-09-09 17:38:17 -0300

    style(ndi): :construction: Converting to uppercase

--- ndi.sh
+++ ndi.sh
@@ -22,5 +22,6 @@ TYPE=$2
 ARCH=$3
 VERSION=$4
 
-url=https://us.download.nvidia.com/${PLATFORM}/${TYPE}-${ARCH}/${VERSION}/NVIDIA-${TYPE}-${ARCH}-${VERSION}.run
-wget $url
+FILE=NVIDIA-${TYPE}-${ARCH}-${VERSION}.run
+URL=https://us.download.nvidia.com/${PLATFORM}/${TYPE}-${ARCH}/${VERSION}/${FILE}
+wget $URL
