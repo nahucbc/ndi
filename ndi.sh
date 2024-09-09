8ac615e15ccb49958135e1cfeb029801397ce4a2
Nahuel Alexis Medina Llanos <nahu.cbc@gmail.com>
2024-09-09 18:07:39 -0300

    feat(ndi): :sparkles: Added support for arm

--- ndi.sh
+++ ndi.sh
@@ -24,4 +24,9 @@ VERSION=$4
 
 FILE=NVIDIA-${TYPE}-${ARCH}-${VERSION}.run
 URL=https://us.download.nvidia.com/${PLATFORM}/${TYPE}-${ARCH}/${VERSION}/${FILE}
+
+if [ $ARCH = "arm" ]; then
+    FILE=NVIDIA-${TYPE}-armv7l-gnueabihf-${VERSION}.run
+    URL=https://us.download.nvidia.com/${PLATFORM}/${TYPE}-x86-ARM/${VERSION}/${FILE}
+fi
 wget $URL
