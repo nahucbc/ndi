7e815304729c00d020944171b205a1850b138604
Nahuel Alexis Medina Llanos <nahu.cbc@gmail.com>
2024-10-03 01:04:38 -0300

    feat(host): :construction: Added match return pattern for enums

--- src/host.rs
+++ src/host.rs
@@ -1,12 +1,31 @@
 enum Platform {
     Linux,
-    FreeBSD,
+    XFreeBSD,
     Solaris
 }
 
 enum Architecture {
-    x86_64,
-    x86,
-    aarch64,
-    arm
+    X86_64,
+    X86,
+    Aarch64,
+    Arm
+}
+
+fn get_platform(platform : &str) -> Option<Platform> {
+    match platform {
+        "Linux" => Some(Platform::Linux),
+        "FreeBSD" => Some(Platform::XFreeBSD),
+        "SunOS" => Some(Platform::Solaris),
+        _ => None
+    }
+}
+
+fn get_architecture(architecture : &str) -> Option<Architecture> {
+    match architecture {
+        "x86_64" => Some(Architecture::X86_64),
+        "i686" => Some(Architecture::X86),
+        "aarch64" => Some(Architecture::Aarch64),
+        "armv7l" => Some(Architecture::Arm),
+        _ => None
+    }
 }
