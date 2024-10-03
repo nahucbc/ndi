a8ba2bbbf0b560cec578dbbd2ca24f8dc3316f08
Nahuel Alexis Medina Llanos <nahu.cbc@gmail.com>
2024-10-03 01:31:35 -0300

    refactor(host): :rotating_light: to public

--- src/host.rs
+++ src/host.rs
@@ -1,17 +1,17 @@
-enum Platform {
+pub enum Platform {
     Linux,
     XFreeBSD,
     Solaris
 }
 
-enum Architecture {
+pub enum Architecture {
     X86_64,
     X86,
     Aarch64,
     Arm
 }
 
-fn get_platform(platform : &str) -> Option<Platform> {
+pub fn get_platform(platform : &str) -> Option<Platform> {
     match platform {
         "Linux" => Some(Platform::Linux),
         "FreeBSD" => Some(Platform::XFreeBSD),
@@ -20,7 +20,7 @@ fn get_platform(platform : &str) -> Option<Platform> {
     }
 }
 
-fn get_architecture(architecture : &str) -> Option<Architecture> {
+pub fn get_architecture(architecture : &str) -> Option<Architecture> {
     match architecture {
         "x86_64" => Some(Architecture::X86_64),
         "i686" => Some(Architecture::X86),
