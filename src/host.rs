fd6c0e20305ae4111c9d872dba8f38e4b2aecb21
Nahuel Alexis Medina Llanos <nahu.cbc@gmail.com>
2024-10-04 00:51:52 -0300

    refactor(host): ♻ Improve api

--- src/host.rs
+++ src/host.rs
@@ -1,28 +1,47 @@
 use uname::uname;
 
-pub enum Platform {
+enum Platform {
     XFree86,
     Solaris
 }
 
 impl Platform {
+
+    fn new(kind : &Kind) -> Option<Platform> {
+        match kind {
+            Kind::Linux => Some(Platform::XFree86),
+            Kind::FreeBSD => Some(Platform::XFree86),
+            Kind::Solaris => Some(Platform::Solaris),
+        }
+    }
     
-    pub fn to_string(&self) -> String {
+    fn to_string(&self) -> String {
         match self {
             Platform::XFree86 => String::from("XFree86"),
             Platform::Solaris => String::from("Solaris")
         }
     }
 }
-pub enum Kind {
+
+#[derive(Clone, Copy)]
+enum Kind {
     Linux,
     FreeBSD,
     Solaris
 }
 
 impl Kind{
+
+    fn new(kind : &str) -> Option<Kind> {
+        match kind {
+            "Linux" => Some(Kind::Linux),
+            "FreeBSD" => Some(Kind::FreeBSD),
+            "SunOS" => Some(Kind::Solaris),
+            _ => None
+        }
+    }
     
-    pub fn to_string(&self) -> String {
+    fn to_string(&self) -> String {
         match self {
             Kind::FreeBSD => String::from("FreeBSD"),
             Kind::Linux => String::from("Linux"),
@@ -31,7 +50,7 @@ impl Kind{
     }
 }
 
-pub enum Architecture {
+enum Architecture {
     X86_64,
     X86,
     Aarch64,
@@ -39,8 +58,18 @@ pub enum Architecture {
 }
 
 impl Architecture {
+
+    fn new(architecture : &str) -> Option<Architecture> {
+        match architecture {
+            "x86_64" => Some(Architecture::X86_64),
+            "i686" => Some(Architecture::X86),
+            "aarch64" => Some(Architecture::Aarch64),
+            "armv7l" => Some(Architecture::Arm),
+            _ => None
+        }
+    }
     
-    pub fn to_string(&self) -> String {
+    fn to_string(&self) -> String {
         match self {
             Architecture::X86_64 => String::from("x86_64"),
             Architecture::X86 => String::from("x86"),
@@ -57,45 +86,16 @@ pub struct Target {
 }
 
 impl Target {
-    pub fn new(platform : Platform, kind : Kind, architecture : Architecture) -> Target {
+
+    pub fn new() -> Target {
+        let host = uname().unwrap();
+        let kind = Kind::new(&host.sysname).unwrap();
+        let platform = Platform::new(&kind).unwrap();
+        let architecture = Architecture::new(&host.machine).unwrap();
         Target {platform, kind, architecture}
     }
-}
 
-
-pub fn get_target() -> Target {
-    let host = uname().unwrap();
-    let kind = get_kind(&host.sysname).unwrap();
-    let platform = get_platform(&kind).unwrap();
-    let architecture = get_architecture(&host.machine).unwrap();
-    let target = Target::new(platform, kind, architecture);
-    return target;
-}
-
-pub fn get_platform(kind : &Kind) -> Option<Platform> {
-    match kind {
-        Kind::Linux => Some(Platform::XFree86),
-        Kind::FreeBSD => Some(Platform::XFree86),
-        Kind::Solaris => Some(Platform::Solaris),
-        _ => None
-    }
-}
-
-pub fn get_kind(kind : &str) -> Option<Kind> {
-    match kind {
-        "Linux" => Some(Kind::Linux),
-        "FreeBSD" => Some(Kind::FreeBSD),
-        "SunOS" => Some(Kind::Solaris),
-        _ => None
-    }
-}
-
-pub fn get_architecture(architecture : &str) -> Option<Architecture> {
-    match architecture {
-        "x86_64" => Some(Architecture::X86_64),
-        "i686" => Some(Architecture::X86),
-        "aarch64" => Some(Architecture::Aarch64),
-        "armv7l" => Some(Architecture::Arm),
-        _ => None
-    }
+    pub fn platform_to_string(&self) -> String {let string =  self.platform.to_string(); string}
+    pub fn kind_to_string(&self) -> String {let string =  self.kind.to_string(); string}
+    pub fn architecture_to_string(&self) -> String {let string =  self.architecture.to_string(); string}
 }
